4115a19cc46fa33c37479f901f32a8f4e4d147b2
IT18126334 <it18126334@my.sliit.lk>
2021-10-01 22:01:14 +0530

    adding index for Home page contents

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Welcome to File Uploader</title>
+  <link href="./img/favicon.png" rel="icon">
+  <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/my-login.css">
+</head>
+<body class="my-login-page" style="background-image: url('./img/back.jpg'); background-attachment:fixed; background-size:cover;">
+<script src="js/jquery.min.js"></script>
+  <script src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
